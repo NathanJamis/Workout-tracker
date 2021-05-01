67819429eee3c3f8098cbc917f36c31ae2c156fc
Nate J <jamis.walsh@gmail.com>
2021-05-01 13:43:03 -0700

    attempting to fix heroku deployment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+const db = require('./models');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
