901025ba981fc4c66e5e0fa6e1049880eef8be01
Nate J <jamis.walsh@gmail.com>
2021-05-01 10:39:18 -0700

    small changes, added .env.EXAMPLE

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
 app.use(require('./routes/api.js'));
 app.use(require('./routes/html.js'));
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server listening on port ${PORT}`)
 });
